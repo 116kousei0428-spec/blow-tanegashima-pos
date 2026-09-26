eef531c73a50563aa5aeb5d344bd29ef28b7cf35
116kousei0428-spec <116kousei0428@gmail.com>
2026-09-26 23:06:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'blow2nd-pos-tanegashima-mobile-v8-cast-register-restored';
+const CACHE_NAME = 'blow2nd-pos-tanegashima-mobile-v9-cast-management-amami-parity';
 const STATIC_ASSETS = [
   './index.html',
   './manifest.json',
